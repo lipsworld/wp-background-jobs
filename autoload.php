574e5caa13d336b0d1e62ac687db734802cb4aa4
sergeliatko <sergeliatko@gmail.com>
2018-07-24 16:16:38 +0200

    Added autoloader

---
+++ autoload.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * PSR-4 Autoloader.
+ */
+spl_autoload_register( function ( $class ) {
+
+	// validate namespace
+	if (
+		strncmp(
+			$namespace = 'SergeLiatko\\WPBackgroundJobs',#namespace
+			$class,
+			$namespace_length = strlen( $namespace )
+		) !== 0
+	) {
+		return;
+	};
+
+	// try to load the file
+	if ( file_exists(
+		$file = join(
+			        DIRECTORY_SEPARATOR,
+			        array(
+				        __DIR__,
+				        'src',#sources directory name
+				        str_replace( '\\', DIRECTORY_SEPARATOR, trim( substr( $class, $namespace_length ), '\\' ) )
+			        )
+		        ) . '.php'
+	) ) {
+		include $file;
+	}
+} );
